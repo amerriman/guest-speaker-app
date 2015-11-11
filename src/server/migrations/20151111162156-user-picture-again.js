39e78797acc8d1bed643a0e7ae21a77580713acf
amerriman <amm434@gmail.com>
2015-11-11 16:32:27 -0700

    added models

---
+++ src/server/migrations/20151111162156-user-picture-again.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.addColumn('Users', 'picture', {
+      type:Sequelize.STRING
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+        return queryInterface.removeColumn('Users', 'picture');
+  }
+};
+
+
